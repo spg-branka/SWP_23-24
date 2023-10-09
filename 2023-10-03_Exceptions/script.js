2a75171215b661807dd63082c065e92936f3208d
newbie677 <97118635+newbie677@users.noreply.github.com>
2023-10-09 19:04:25 +0200

    hw exceptions

---
+++ 2023-10-03_Exceptions/script.js
@@ -0,0 +1,16 @@
+function myFunction(randomNum) {
+    if (randomNum < 0.5) {
+        //console.log("Error!");
+        throw new Error("randomNum kleiner als 0.5");
+    }
+    return "Result: " + randomNum;
+}
+
+for (let i = 0; i < 15; i++) {
+    let randomNum = Math.random();
+    try {
+        console.log(myFunction(randomNum));
+    } catch (error) {
+        console.error("Exception: " + error.message);
+    }
+}
